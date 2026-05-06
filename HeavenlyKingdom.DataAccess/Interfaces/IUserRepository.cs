b33ecebd98a20b2aa6274a684cb1933765285df6
vlr404 <valeratitov443@gmail.com>
2026-05-06 17:11:31 +0300

    feat: расширена модель User, переход с username на email-аутентификацию, добавлен logout

--- HeavenlyKingdom.DataAccess/Interfaces/IUserRepository.cs
+++ HeavenlyKingdom.DataAccess/Interfaces/IUserRepository.cs
@@ -5,7 +5,7 @@ namespace HeavenlyKingdom.DataAccess.Interfaces
     public interface IUserRepository
     {
         Task<User?> GetByIdAsync(int id);
-        Task<User?> GetByUsernameAsync(string username);
+        Task<User?> GetByEmailAsync(string email);
         Task<IEnumerable<User>> GetAllAsync();
         Task AddAsync(User user);
         Task UpdateAsync(User user);
