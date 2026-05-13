b4fb9cf3aca41dfc6753f478ae0693aca72b342b
vlr404 <valeratitov443@gmail.com>
2026-05-13 11:29:55 +0300

    refactor:добавление явных атрибутов аннотации данных и навигационных свойств в доменные сущности

--- HeavenlyKingdom.Domain/Entities/Address.cs
+++ HeavenlyKingdom.Domain/Entities/Address.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace HeavenlyKingdom.Domain.Entities
 {
     public class Address
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         public string City { get; set; } = string.Empty;
         public string Street { get; set; } = string.Empty;
         public string House { get; set; } = string.Empty;
@@ -10,6 +16,11 @@ namespace HeavenlyKingdom.Domain.Entities
         public bool IsDefault { get; set; } = false;
 
         public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
         public User User { get; set; } = null!;
+
+        [InverseProperty("Address")]
+        public List<Order> Orders { get; set; } = new();
     }
 }
--- HeavenlyKingdom.Domain/Entities/Category.cs
+++ HeavenlyKingdom.Domain/Entities/Category.cs
@@ -1,11 +1,18 @@
-﻿namespace HeavenlyKingdom.Domain.Entities
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HeavenlyKingdom.Domain.Entities
 {
     public class Category
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         public string Name { get; set; } = string.Empty;
         public string Slug { get; set; } = string.Empty;
 
+        [InverseProperty("Category")]
         public ICollection<Product> Products { get; set; } = new List<Product>();
     }
 }
